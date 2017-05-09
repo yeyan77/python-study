60a11736578be5d436693149374ab6198325e41a
xujing <xujing@yuantiaotech.com>
2017-05-09 11:10:11 +0800

    add3

--- test/test.py
+++ test/test.py
@@ -140,6 +140,8 @@ else:
 """
 
 #获取指定期间内的阿姆斯特朗数
+
+"""
 lower = int(input('lower:'))
 upper = int(input('upper:'))
 
@@ -155,3 +157,38 @@ for num in range(lower,upper+1):
 
     if num == sum:
         print(num)
+"""
+
+# 引入日历模块
+
+"""
+import calendar
+
+yy = int(input('yy:'))
+mm = int(input('mm:'))
+
+print(calendar.month(yy,mm))
+"""
+
+# 写文件
+"""
+with open("test.txt","wt") as out_file:
+    out_file.write("该文本会写入到文件中\n看到我了吧！")
+
+with open("test.txt","rt") as in_file:
+    text = in_file.read()
+
+    print(text)
+"""
+
+print ("Content-type:text/html")
+print ()                             # 空行，告诉服务器结束头部
+print ('<html>')
+print ('<head>')
+print ('<meta charset="utf-8">')
+print ('<title>Hello Word - 我的第一个 CGI 程序！</title>')
+print ('</head>')
+print ('<body>')
+print ('<h2>Hello Word! 我是来自菜鸟教程的第一CGI程序</h2>')
+print ('</body>')
+print ('</html>')
